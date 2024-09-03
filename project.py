6a4b152a809444009c637fe07e6240d044de1415
sidney2k1 <sidsingla02@gmail.com>
2024-09-03 17:48:31 +0530

    project

---
+++ project.py
@@ -0,0 +1,6 @@
+name=str(input("Enter your name:"))
+age=int(input("Enter your age:"))
+if age>=10 and age<=20:
+    print("you are allowed in class")
+elif age<10 or age>20:
+    print("you are not allowed in class")
